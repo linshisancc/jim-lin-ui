b6d0dd9285401c0fe69aeb7e0b5d560f93eca483
黄奥林 <huangaolin@beyondsoft.com>
2022-08-31 17:42:06 +0800

    init: 初始化仓库

---
+++ src/Alert/index.tsx
@@ -0,0 +1,29 @@
+import pt from 'prop-types';
+import React from 'react';
+
+export interface IAlertProps extends React.PropsWithChildren {
+  type?: 'info' | 'positive' | 'negative' | 'warning';
+}
+
+export type typeColorMap = Record<Required<IAlertProps>['type'], string>;
+
+const prefixCls = 'common-alert';
+
+const colors: typeColorMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert: React.FC<IAlertProps> = ({ children, type = 'info', ...others }) => (
+  <div className={prefixCls} style={{ background: colors[type] }} {...others}>
+    {children}
+  </div>
+);
+
+Alert.propTypes = {
+  type: pt.oneOf(['info', 'positive', 'negative', 'warning']),
+};
+
+export default Alert;
