7027e98160b6497b5f5e2201fa5e9eb526165881
黄奥林 <huangaolin@beyondsoft.com>
2022-08-31 17:44:25 +0800

    chore: 增加配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['ci', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']],
+    'type-enum': [2, 'always', ['ci', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'init']],
     'type-case': [0],
     'type-empty': [0],
     'scope-empty': [0],
